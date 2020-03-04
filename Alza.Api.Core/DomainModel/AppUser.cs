003dd2782a48e410a56a4063325cf2557ccb9143
Tomáš Walek <tomass.wal@gmail.com>
2020-03-04 22:16:42 +0100

    add ef dbcontext with indentity

---
+++ Alza.Api.Core/DomainModel/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alza.Api.Core.DomainModel
+{
+    public class AppUser : IdentityUser<long>, IEntity<long>
+    {
+    }
+}
---
+++ Alza.Api.Infrastructure/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Alza.Api.Core.DomainModel;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alza.Api.Infrastructure
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, long>
+    {
+    }
+}
