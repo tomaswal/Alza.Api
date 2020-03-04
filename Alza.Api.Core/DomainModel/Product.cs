461423dd104c9b9c2b1a84b4a8fd070eb48bf711
Tomáš Walek <tomass.wal@gmail.com>
2020-03-04 22:33:18 +0100

    Create dbcontext

--- Alza.Api.Core/DomainModel/Product.cs
+++ Alza.Api.Core/DomainModel/Product.cs
@@ -6,7 +6,10 @@ namespace Alza.Api.Core.DomainModel
 {
     public class Product : IEntity<long>
     {
-        List<int> t;
         public long Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUri{get; set;}
+        public decimal Price { get; set; }
+        public string Description { get; set; }
     }
 }
--- Alza.Api.Infrastructure/AppDbContext.cs
+++ Alza.Api.Infrastructure/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Alza.Api.Core.DomainModel;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,48 @@ namespace Alza.Api.Infrastructure
 {
     public class AppDbContext : IdentityDbContext<AppUser, AppRole, long>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.ImgUri)
+                .IsRequired();
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .IsRequired();
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .IsRequired();
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Product>()
+                .HasData(new Product()
+                {
+                    Id = 1,
+                    ImgUri = "/Images/notebook.png",
+                    Name = "Notebook",
+                    Description = "Beautiful notebook",
+                    Price = 29_999.77m
+                },
+                new Product()
+                {
+                    Id = 2,
+                    ImgUri = "/Images/mobile.png",
+                    Name = "Mobile",
+                    Description = "Old mobile",
+                    Price = 1_999.99m
+                });
+        }
     }
 }
