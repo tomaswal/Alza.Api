eddf1aa0d5d2866d28118ce5e24ab99deb4ecf77
Tomáš Walek <tomass.wal@gmail.com>
2020-03-04 22:50:27 +0100

    Add test library

---
+++ Alza.Api.Core.Tests/BussinesRule/ProductFacadeTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alza.Api.Core.Tests.BussinesRule
+{
+    [TestClass]
+    public class ProductFacadeTests
+    {
+    }
+}
