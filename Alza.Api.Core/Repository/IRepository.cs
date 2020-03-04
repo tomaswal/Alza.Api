643fecc3e2fe57d1c5dae17b43a26266f8966ef0
Tomáš Walek <tomass.wal@gmail.com>
2020-03-04 22:08:02 +0100

    Create base repository interface

---
+++ Alza.Api.Core/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Alza.Api.Core.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alza.Api.Core.Repository
+{
+    public interface IRepository<T> where T : struct
+    {
+        ICollection<IEntity<T>> FindAll();
+        IEntity<T> FindById(T id);
+        void Remove(IEntity<T> entity);
+    }
+}
