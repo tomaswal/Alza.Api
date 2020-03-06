56f4419933981f64912ad474a7cb01d93e9643f3
Tomáš Walek <tomass.wal@gmail.com>
2020-03-06 22:19:03 +0100

    add swagger

--- Alza.Api/v1/Controllers/ValuesController.cs
+++ Alza.Api/v1/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Alza.Api.Controllers
+namespace Alza.Api.v1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
