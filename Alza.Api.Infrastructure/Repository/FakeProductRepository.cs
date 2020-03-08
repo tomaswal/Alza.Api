d96c452754d51ce1794d5392fd43d5bb25bb10d9
Tomáš Walek <tomass.wal@gmail.com>
2020-03-08 12:09:43 +0100

    Add fake productrepository

---
+++ Alza.Api.Infrastructure/Repository/FakeProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Alza.Api.Core.DomainModel;
+using Alza.Api.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alza.Api.Infrastructure.Repository
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public ICollection<Product> FindAll()
+        {
+            var result = new List<Product>();
+            result.Add(new Product
+            {
+                Id = 1,
+                Description = "desc1",
+                ImgUri = "/test1/test1/test1.png",
+                Name = "name1",
+                Price = 1.1M
+            });
+            result.Add(new Product
+            {
+                Id = 2,
+                Description = "desc2",
+                ImgUri = "/test2/test2/test2.png",
+                Name = "name2",
+                Price = 2.2M
+            });
+
+            return result;
+        }
+
+        public Product FindById(long id)
+        {
+            return new Product
+            {
+                Id = 1,
+                Description = "desc1",
+                ImgUri = "/test1/test1/test1.png",
+                Name = "name1",
+                Price = 1.1M
+            };
+        }
+
+        public void Remove(Product entity)
+        {
+        }
+    }
+}
--- Alza.Api/Startup.cs
+++ Alza.Api/Startup.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Alza.Api.Core.Repository;
+using Alza.Api.Infrastructure;
+using Alza.Api.Infrastructure.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,12 +29,9 @@ namespace Alza.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddMvcCore()
                 .AddJsonFormatters()
@@ -58,19 +58,27 @@ namespace Alza.Api
                         });
                     }
                 });
-        }
 
+            RegisterAppClases(services);
+
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
+
+            //using (var scope = app.ApplicationServices.CreateScope())
+            //{
+            //    var identityAppContext = scope.ServiceProvider.GetService<AppDbContext>();
+            //    identityAppContext.Database.Migrate();
+            //}
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -85,6 +93,11 @@ namespace Alza.Api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private void RegisterAppClases(IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, FakeProductRepository>();
         }
     }
 }
